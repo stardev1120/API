edbe2ca5003e1f93e3b45ea11b32f4ce1e7a0f75
Mohamed Mamdouh <mmamdouh86@gmail.com>
2018-01-15 10:31:52 +0200

    clean up migration

--- API-stage/node_app/migrations/20170517115819-insert-countries.js
+++ API-stage/node_app/migrations/20170517115819-insert-countries.js
@@ -2,15 +2,13 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Countries', [{
-      name: 'some country',
-      country_code: 'SG',
-      status: 1
-    }, {
-      name: 'ukraine',
-      country_code: 'UA',
-      status: 1
-    }]);
+      return queryInterface.sequelize.query("INSERT INTO Countries (id, name, country_code, status) " +
+          "VALUES(1, 'some country', 'SG', 1) " +
+          "ON DUPLICATE KEY UPDATE  name='some country', country_code='SG', status=1").then(()=>{
+              return queryInterface.sequelize.query("INSERT INTO Countries (id, name, country_code, status) " +
+                  "VALUES(2, 'ukraine', 'UA', 1) " +
+                  "ON DUPLICATE KEY UPDATE  name='ukraine', country_code='UA', status=1").then()
+          });
   },
 
   down: function (queryInterface, Sequelize) {
